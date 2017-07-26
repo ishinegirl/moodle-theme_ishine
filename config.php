ed8ce9db9cfb0e16f21ad6271f753332a61acefd
justinhunt <bitwalkerjapan@gmail.com>
2017-07-26 18:16:29 +0900

    initial commit of ishine theme

---
+++ config.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Boost config.
+ *
+ * @package   theme_ishine
+ * @copyright 2017 Justin Hunt
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->dirroot . '/theme/boost/lib.php');
+require_once($CFG->dirroot . '/theme/ishine/lib.php');
+
+$THEME->name = 'ishine';
+$THEME->sheets = [''];
+$THEME->editor_sheets = [''];
+
+$THEME->parents = ['boost'];
+
+// Call main theme scss from parent- including the selected preset.
+$THEME->scss = function($theme) {
+    return theme_ishine_get_main_scss_content($theme);
+};
+
+// Docking is not currently supported in Boost family themes.
+$THEME->enable_dock = false;
+
+// Call css/scss processing functions from Boost.
+$THEME->csstreepostprocessor = 'theme_boost_css_tree_post_processor';
+$THEME->prescsscallback = 'theme_boost_get_pre_scss';
+$THEME->extrascsscallback = 'theme_boost_get_extra_scss';
+
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+
+// Additional theme options.
+$THEME->supportscssoptimisation = false;
+$THEME->yuicssmodules = array();
+$THEME->undeletableblocktypes = '';
---
+++ lang/en/theme_ishine.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_ishine
+ * @copyright 2016 Frédéric Massart
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'ishine is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
+$string['currentinparentheses'] = '(current)';
+$string['configtitle'] = 'ishine';
+$string['pluginname'] = 'ishine';
+$string['region-side-pre'] = 'Right';
+
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Advanced settings';
+// The brand colour setting.
+$string['brandcolor'] = 'Brand colour';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'The accent colour.';
+
+// Name of the first settings tab.
+$string['generalsettings'] = 'General settings';
+
+// Preset files setting.
+$string['presetfiles'] = 'Additional theme preset files';
+// Preset files help text.
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+// Preset setting.
+$string['preset'] = 'Theme preset';
+// Preset help text.
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+// Raw SCSS setting.
+$string['rawscss'] = 'Raw SCSS';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'Raw initial SCSS';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
